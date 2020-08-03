325bd6de5c0f9cf0ffc02a7abc938aaf04396710
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-08-02 17:09:52 -0700

    add `+++` and `reverse2` custom functions

--- chapter2/src/Chapter2/Section3/SimpleFunctions.hs
+++ chapter2/src/Chapter2/Section3/SimpleFunctions.hs
@@ -2,3 +2,12 @@ module Chapter2.Section3.SimpleFunctions where
 
 firstOrEmpty :: [[Char]] -> [Char]
 firstOrEmpty lst = if not (null lst) then head lst else "empty"
+
+
+lst1 +++ lst2 = if null lst1 {- check emptiness -}
+                   then lst2 -- base case
+                   else (head lst1) : (tail lst1 +++ lst2)
+
+reverse2 list = if null list
+                   then []
+                   else reverse2 (tail list) +++ [head list]
