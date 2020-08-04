dd4458840afb30fb9e78e6c3a35c479f79e356b8
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-08-03 19:37:58 -0700

    add 2-4 exercise's answer

--- chapter2/src/Chapter2/Section3/SimpleFunctions.hs
+++ chapter2/src/Chapter2/Section3/SimpleFunctions.hs
@@ -17,11 +17,16 @@ reverse2 list =
     else reverse2 (tail list) +++ [head list]
 
 maxmin list =
-  if null (tail list)
-    then (head list, head list)
-    else ( if head list > fst (maxmin (tail list))
-             then head list
-             else fst (maxmin (tail list))
-         , if head list < snd (maxmin (tail list))
-             then head list
-             else snd (maxmin (tail list)))
+  let h = head list
+   in if null (tail list)
+        then (h, h)
+        else ( if h > t_max
+                 then h
+                 else t_max
+             , if h < t_min
+                 then h
+                 else t_min)
+  where
+    t = maxmin (tail list)
+    t_max = fst t
+    t_min = snd t
---
+++ chapter2/src/Chapter2/Section4/DataTypes.hs
@@ -0,0 +1,42 @@
+module Chapter2.Section4.DataTypes where
+
+data Client
+  = GovOrg String
+  | Company String Integer Person String
+  | Individual Person Bool
+  deriving (Show)
+
+data Person =
+  Person String String Gender
+  deriving (Show)
+
+data Gender
+  = Male
+  | Female
+  | Unknown
+  deriving (Show)
+
+data TimeMachine =
+  TimeMachine Manufacturer Model Name Direction Price
+  deriving (Show)
+
+newtype Manufacturer =
+  Manufacturer String
+  deriving (Show)
+
+newtype Model =
+  Model Int
+  deriving (Show)
+
+newtype Name =
+  Name String
+  deriving (Show)
+
+data Direction
+  = PAST
+  | FUTURE
+  deriving (Show)
+
+newtype Price =
+  Price Float
+  deriving (Show)
