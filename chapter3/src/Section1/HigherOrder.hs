77728327964abf23cc7fbed880ff0ac375c2a7aa
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-08-08 13:06:43 -0700

    chapter 3 data types with parameteric polymorphism

--- chapter3/src/Section1/HigherOrder.hs
+++ chapter3/src/Section1/HigherOrder.hs
@@ -2,3 +2,35 @@ module HigherOrder where
 
 maybeString (Just _) = "Just"
 maybeString Nothing = "Nothing"
+
+data Client i
+  = GovOrg
+      { clientId :: i
+      , clientName :: String
+      }
+  | Company
+      { clientId :: i
+      , clientName :: String
+      , person :: Person
+      , duty :: String
+      }
+  | Individual
+      { clientId :: i
+      , person :: Person
+      }
+  deriving (Show, Eq, Ord)
+
+data Person =
+  Person
+    { firstName :: String
+    , lastName :: String
+    }
+  deriving (Show, Eq, Ord)
+
+data Triple a b c =
+  Triple a b c
+  deriving (Show)
+
+data SamePair a =
+  SamePair a a
+  deriving (Show)
