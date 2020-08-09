c87e427c95e7eed98fb56ef8990cbd732c249ff3
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-08-08 21:43:31 -0700

    section3

--- chapter3/src/Chapter3/Section3/MoreModules.hs
+++ chapter3/src/Chapter3/Section3/MoreModules.hs
@@ -1,4 +1,11 @@
-module Chapter3.Section3.MoreModules where
+{-# LANGUAGE PatternSynonyms #-}
+
+module Chapter3.Section3.MoreModules
+  ( Range()
+  , range
+  --, RangeObs(..)
+  --, r
+  ) where
 
 import Chapter3.Section1.HigherOrder (Client(GovOrg)) -- Client type and GovOrg constructor only
 
@@ -16,3 +23,30 @@ permutationsStartingWith' :: Char -> String -> [String]
   Data.List.filter (\l -> head l == letter) . Data.List.permutations-}
 permutationsStartingWith' letter =
   L.filter (\l -> head l == letter) . L.permutations
+
+-- smart constructor example
+data Range =
+  Range Integer Integer
+  deriving (Show)
+
+range :: Integer -> Integer -> Range
+range a b =
+  if a <= b
+    then Range a b
+    else error "a must be <= b"
+        {-data RangeObs =
+  R Integer Integer
+  deriving (Show)
+
+r :: Range -> RangeObs
+r (Range a b) = R a b -}
+
+prettyRange :: Range -> String
+prettyRange rng =
+  case rng of
+    Range a b -> "[" ++ show a ++ "," ++ show b ++ "]"
+
+pattern R :: Integer -> Integer -> Range
+
+pattern R a b <- Range a b
+  where R a b = range a b
