5e0c6a91f2c1ed9d9b12594eb19e4e9b16b12dea
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-08-09 19:51:08 -0700

    Ex 3-3

--- chapter3/src/Chapter3/Section4/Lists.hs
+++ chapter3/src/Chapter3/Section4/Lists.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE NamedFieldPuns, LambdaCase #-}
 
 module Chapter3.Section4.Lists where
 
@@ -23,13 +23,13 @@ infMax PlusInfinity _ = PlusInfinity
 infMax _ PlusInfinity = PlusInfinity
 infMax (Number a) (Number b) = Number (max a b)
 
-foldr' :: (a -> b -> b) -> b -> [a] -> b
-foldr' _ initial [] = initial
-foldr' f initial (x:xs) = f x (foldr' f initial xs)
+foldr'' :: (a -> b -> b) -> b -> [a] -> b
+foldr'' _ initial [] = initial
+foldr'' f initial (x:xs) = f x (foldr'' f initial xs)
 
-foldl' :: (a -> b -> a) -> a -> [b] -> a
-foldl' _ initial [] = initial
-foldl' f initial (x:xs) = foldl' f (f initial x) xs
+foldl'' :: (a -> b -> a) -> a -> [b] -> a
+foldl'' _ initial [] = initial
+foldl'' f initial (x:xs) = foldl'' f (f initial x) xs
 
 {- HLint Ignore maximum -}
 maximum' :: [Integer] -> Integer
@@ -52,3 +52,39 @@ minimumClient' (x:xs) = min (minimumClient' [x]) (minimumClient' xs)
 all' :: [Bool] -> Bool
 all' [] = True
 all' (x:xs) = x && all' xs
+
+-- using foldr
+product'' :: Num a => [a] -> a
+product'' xs = foldr (*) 1 xs
+
+all'' :: [Bool] -> Bool
+all'' xs = foldr (&&) True xs
+
+minimumClient'' :: [Client a] -> Int
+minimumClient'' [] = 0
+minimumClient'' (x:xs) = foldr (\x y -> min y (getLength x)) (getLength x) xs
+
+-- using foldl
+product''' :: Num a => [a] -> a
+product''' xs = foldl (*) 1 xs
+
+all''' :: [Bool] -> Bool
+all''' xs = foldl (&&) True xs
+
+minimumClient''' :: [Client a] -> Int
+minimumClient''' [] = 0
+minimumClient''' (x:xs) = foldl (\x y -> min x (getLength y)) (getLength x) xs
+
+getLength client =
+  case client of
+    GovOrg {clientName} -> length clientName
+    Company {clientName} -> length clientName
+    Individual {person = Person {firstName}} -> length firstName
+
+minimumBy f xs =
+  foldr1
+    (\x y ->
+       if (f x) < (f y)
+         then x
+         else y)
+    xs
