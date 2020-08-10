f71580a21c93180cc9be9966d5cc9fb142c09448
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-08-09 17:42:49 -0700

    add errata and section4 exercises

--- chapter3/src/Chapter3/Section4/Lists.hs
+++ chapter3/src/Chapter3/Section4/Lists.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Chapter3.Section4.Lists where
+
+import Chapter3.Section1.HigherOrder (Client(..), Person(..))
 
 filter' :: (a -> Bool) -> [a] -> [a]
 filter' _ [] = []
@@ -6,3 +10,45 @@ filter' f (x:xs) =
   if f x
     then x : filter' f xs
     else filter' f xs
+
+data InfNumber a
+  = MinusInfinity
+  | Number a
+  | PlusInfinity
+  deriving (Show)
+
+infMax MinusInfinity x = x
+infMax x MinusInfinity = x
+infMax PlusInfinity _ = PlusInfinity
+infMax _ PlusInfinity = PlusInfinity
+infMax (Number a) (Number b) = Number (max a b)
+
+foldr' :: (a -> b -> b) -> b -> [a] -> b
+foldr' _ initial [] = initial
+foldr' f initial (x:xs) = f x (foldr' f initial xs)
+
+foldl' :: (a -> b -> a) -> a -> [b] -> a
+foldl' _ initial [] = initial
+foldl' f initial (x:xs) = foldl' f (f initial x) xs
+
+{- HLint Ignore maximum -}
+maximum' :: [Integer] -> Integer
+maximum' = foldr1 max
+
+-- Ex 3-3
+product' :: Num a => [a] -> a
+product' [] = 1
+product' [x] = x
+product' (x:y:zs) = x * y * (product' zs)
+
+minimumClient' :: [Client a] -> Int
+minimumClient' [x] =
+  case x of
+    GovOrg {clientName} -> length clientName
+    Company {clientName} -> length clientName
+    Individual {person = Person {firstName}} -> length firstName
+minimumClient' (x:xs) = min (minimumClient' [x]) (minimumClient' xs)
+
+all' :: [Bool] -> Bool
+all' [] = True
+all' (x:xs) = x && all' xs
