efbe03870f906409d11804281d57be07bcb07a35
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-08-14 18:41:26 -0700

    add chapter 4

--- chapter4/src/Chapter4/Section1/Containers.hs
+++ chapter4/src/Chapter4/Section1/Containers.hs
@@ -3,6 +3,7 @@
 module Chapter4.Section.Containers where
 
 import qualified Data.Map as M
+import qualified Data.Set as S
 
 insert :: Ord k => k -> a -> M.Map k a -> M.Map k a
 insert k v =
@@ -23,3 +24,35 @@ adjust f =
   M.alter
     (\case
        (Just v) -> Just (f v))
+
+data Client i
+  = GovOrg
+      { clientId :: i
+      , clientName :: String
+      }
+  | Company
+      { clientId :: i
+      , clientName :: String
+      , person :: Person
+      , duty :: String
+      }
+  | Individual
+      { clientId :: i
+      , person :: Person
+      }
+  deriving (Show, Eq, Ord)
+
+data Person =
+  Person
+    { firstName :: String
+    , lastName :: String
+    }
+  deriving (Show, Eq, Ord)
+
+data ClientKind
+  = GovOrgKind
+  | CompanyKind
+  | IndividualKind
+
+classifyClients :: [Client Integer] -> M.Map ClientKind (S.Set (Client Integer))
+classifyClients [] = M.empty
