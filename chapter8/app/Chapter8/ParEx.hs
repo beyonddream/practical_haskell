b72df295c9b93511143c1b84b5b9fa6604c5b41e
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-10-05 21:56:09 -0700

    Solution to Ex 8-1

--- chapter8/app/Chapter8/ParEx.hs
+++ chapter8/app/Chapter8/ParEx.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+
 module Chapter8.ParEx where
 
 import Control.DeepSeq
 import Control.Monad.Par
+import Data.List
+import qualified Data.Map as M
 import Data.Set (Set)
 import qualified Data.Set as S
 
@@ -201,3 +205,79 @@ filterLk minSupp transactions ck =
                     rVar <- spawn $ filterLk minSupp transactions r
                     rFiltered <- get rVar
                     return $ lFiltered ++ rFiltered
+
+--
+-- Ex 8-1
+--
+class Ord v =>
+      Vector v
+  where
+  distance :: v -> v -> Double
+  centroid :: [v] -> v
+
+instance Vector (Double, Double) where
+  distance (a, b) (c, d) = sqrt $ (c - a) * (c - a) + (d - b) * (d - b)
+  centroid lst =
+    let (u, v) = foldr (\(a, b) (c, d) -> (a + c, b + d)) (0, 0) lst
+        n = fromIntegral $ length lst
+     in (u / n, v / n)
+
+class Vector v =>
+      Vectorizable e v
+  where
+  toVector :: e -> v
+
+instance Vectorizable (Double, Double) (Double, Double) where
+  toVector = id
+
+shouldStop :: (Vector v) => [(v, v)] -> Double -> Bool
+shouldStop centroids threshold =
+  foldr (\(x, y) s -> s + distance x y) 0.0 centroids < threshold
+
+clusterAssignmentPhase ::
+     (Ord v, Vector v, Vectorizable e v) => [v] -> [e] -> M.Map v [e]
+clusterAssignmentPhase centroids points =
+  let initialMap = M.fromList $ zip centroids (repeat [])
+   in foldr
+        (\p m ->
+           let chosenC = minimumBy (compareDistance p) centroids
+            in M.adjust (p :) chosenC m)
+        initialMap
+        points
+  where
+    compareDistance p x y =
+      compare (distance x $ toVector p) (distance y $ toVector p)
+
+newCentroidPhase :: (Vector v, Vectorizable e v) => M.Map v [e] -> [(v, v)]
+newCentroidPhase = M.toList . fmap (centroid . map toVector)
+
+kMeans ::
+     (Ord v, Vector v, Vectorizable e v, NFData v)
+  => (Int -> [e] -> [v]) -- initialization function
+  -> Int -- number of centroids
+  -> [e] -- the information
+  -> Double -- threshold
+  -> ([v], Int) -- final centroids with no of iterations
+kMeans i k points threshold = kMeans' (i k points) points threshold 1
+
+kMeans' ::
+     (Ord v, Vector v, Vectorizable e v, NFData v)
+  => [v]
+  -> [e]
+  -> Double
+  -> Int
+  -> ([v], Int)
+kMeans' centroids points threshold depth =
+  runPar $ do
+    let assignments = clusterAssignmentPhase centroids points
+        oldNewCentroids = newCentroidPhase assignments
+    newCentroids <- parMap snd oldNewCentroids
+    if shouldStop oldNewCentroids threshold
+      then return (newCentroids, depth)
+      else return $ kMeans' newCentroids points threshold (depth + 1)
+
+-- test kmeans
+initializeSimple :: Int -> [e] -> [(Double, Double)]
+initializeSimple 0 _ = []
+initializeSimple n v =
+  (fromIntegral n, fromIntegral n) : initializeSimple (n - 1) v
--- chapter8/app/Main.hs
+++ chapter8/app/Main.hs
@@ -13,3 +13,5 @@ main = do
       3
       [(1, "Client1"), (2, "Client2")]
       [(3, "product3"), (5, "product5")]
+  let info = [(1, 1), (1, 2), (4, 4), (4, 5)] :: [(Double, Double)]
+   in print $ kMeans initializeSimple 2 info 0.001
