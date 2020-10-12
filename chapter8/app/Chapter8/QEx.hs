f52f5201f0a680c9e371c7701bd4f1da0a623241
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-10-11 17:05:19 -0700

    Solution for Ex 8-3 (there is some issue with this code)

--- chapter8/app/Chapter8/QEx.hs
+++ chapter8/app/Chapter8/QEx.hs
@@ -3,26 +3,68 @@ module Chapter8.QEx where
 import Chapter8.STMEx
 import Control.Concurrent
 import Control.Concurrent.STM
+import Control.Concurrent.STM.Delay
 import Control.Monad
-
-main :: IO ()
+import Data.List
+import Data.Maybe
+import Debug.Trace
+import System.Random
+        {-main :: IO ()
 main = do
   q <- newTQueueIO
   _ <- forkIO $ backend q
   replicateM_ 10 $ forkIO (frontend q)
   _ <- getLine
-  return ()
-
-backend :: TQueue (String, Integer) -> IO ()
+  return ()-}
+        {-backend :: TQueue (String, Integer) -> IO ()
 backend q = do
   m <- newTVarIO 10000
   s <- newTVarIO [("a", 7)]
   forever $
     atomically $ do
       (product', price) <- readTQueue q
-      updateMoneyAndStock product' price m s
+      return ()
 
 frontend :: TQueue (String, Integer) -> IO ()
 frontend q = do
   (product'', price) <- undefined
-  atomically $ writeTQueue q (product'', price)
+  atomically $ writeTQueue q (product'', price)-}
+
+timeMachineRequestor ::
+     TBQueue (String, Integer, TVar [Integer], Delay)
+  -> String
+  -> Integer
+  -> TVar [Integer]
+  -> Delay
+  -> IO ()
+timeMachineRequestor q name year travelYears travelDuration =
+  atomically $ writeTBQueue q (name, year, travelYears, travelDuration)
+
+timeMachineAllocator ::
+     TBQueue (String, Integer, TVar [Integer], Delay) -> IO ()
+timeMachineAllocator q =
+  forever $
+  atomically $ do
+    (name, year, travelYears, wait) <- readTBQueue q
+    _ <- startTravel' name travelYears year
+    _ <- waitDelay wait
+    _ <- endTravel' name travelYears year
+    return ()
+
+startTravel' :: String -> TVar [Integer] -> Integer -> STM ()
+startTravel' name travelYears year = do
+  years <- readTVar travelYears
+  when (isJust (find (== year) years)) (traceM ("retrying" ++ name) >> retry)
+  writeTVar travelYears (insert year years)
+  y' <- readTVar travelYears
+  traceM
+    ("starting travel for : " ++ show name ++ " " ++ show year ++ " " ++ show y')
+
+endTravel' :: String -> TVar [Integer] -> Integer -> STM ()
+endTravel' name travelYears year = do
+  years <- readTVar travelYears
+  writeTVar travelYears (delete year years)
+  y' <- readTVar travelYears
+  traceM
+    ("ending travel for: " ++
+     show name ++ " " ++ show year ++ " " ++ " " ++ show y')
--- chapter8/app/Main.hs
+++ chapter8/app/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Chapter8.QEx
 import Chapter8.STMEx
 import Control.Concurrent
 import Control.Concurrent.STM
@@ -8,6 +9,19 @@ import System.IO
 import System.Random
 
 main :: IO ()
+main = do
+  q <- newTBQueueIO 3
+  travelYears <- newTVarIO []
+  r <- randomRIO (1, 3)
+  travelDuration <- newDelay (r * 1000000)
+  _ <- forkIO $ timeMachineAllocator q
+  _ <- forkIO $ timeMachineRequestor q "tm1" 2021 travelYears travelDuration
+  _ <- forkIO $ timeMachineRequestor q "tm2" 2022 travelYears travelDuration
+  _ <- forkIO $ timeMachineRequestor q "tm3" 2022 travelYears travelDuration
+  _ <- forkIO $ timeMachineRequestor q "tm4" 2023 travelYears travelDuration
+  _ <- getLine
+  return ()
+        {-main :: IO ()
 main = do
   capacity <- newTVarIO 0
   travelYears <- newTVarIO []
@@ -21,3 +35,4 @@ main = do
     performTimeTravel "tm3" 2022 capacity 2 travelYears travelDuration
   _ <- getLine
   return ()
+  -}
