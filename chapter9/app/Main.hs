1aae00b47223a56edb8343ad7c330dfef83e5a2a
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-10-13 22:02:28 -0700

    update with another example

--- chapter9/app/Main.hs
+++ chapter9/app/Main.hs
@@ -1,4 +1,49 @@
 module Main where
 
+import System.IO
+
 main :: IO ()
-main = putStrLn "Hello, Beginning Haskell!"
+main
+    --print1
+ = print2
+
+print2 :: IO ()
+print2 = do
+  putStrLn "First name?"
+  fName <- getLine
+  putStrLn "Last name?"
+  lName <- getLine
+  putChar '>' >> putChar ' '
+  print $ Person fName lName
+
+data Person =
+  Person
+    { firstName :: String
+    , lastName :: String
+    }
+  deriving (Show, Eq, Ord)
+
+data Client i
+  = GovOrg
+      { clientId :: i
+      , clientName :: String
+      }
+  | Company
+      { clientId :: i
+      , clientName :: String
+      , person :: Person
+      , duty :: String
+      }
+  | Individual
+      { clientId :: i
+      , person :: Person
+      }
+  deriving (Show, Eq, Ord)
+
+print1 :: IO ()
+print1 = do
+  hSetBuffering stdout LineBuffering
+  putStrLn "Where do you want to travel?"
+  place <- getLine
+  let year = length place * 10
+  putStrLn $ "You should travel to year " ++ show year
