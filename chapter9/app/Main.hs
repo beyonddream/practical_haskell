542998a9c614621253004b8b16e2f4b3a9cb6e0d
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-10-17 13:18:46 -0700

    randomness example

--- chapter9/app/Main.hs
+++ chapter9/app/Main.hs
@@ -1,11 +1,23 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import Control.Monad.Loops
 import System.IO
+import System.Random
 
 main :: IO ()
-main
-    --print1
- = print2
+main = do
+  (initial :: Int) <- fmap read getLine
+  jumps <-
+    unfoldrM
+      (\_ -> do
+         next <- randomRIO (0, 3000)
+         if next == initial
+           then return Nothing
+           else return $ Just (next, next))
+      initial
+  print $ take 10 jumps
 
 print2 :: IO ()
 print2 = do
