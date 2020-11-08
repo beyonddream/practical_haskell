50afbb6e880e620aa3f1266c4bf7934a3988c3dc
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-11-08 15:57:10 -0800

    sample product/id route

--- chapter12/app/Chapter12/Database.hs
+++ chapter12/app/Chapter12/Database.hs
@@ -1,1 +1,43 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell, QuasiQuotes, TypeFamilies,
+  MultiParamTypeClasses, FlexibleContexts, GADTs, OverloadedStrings,
+  GeneralizedNewtypeDeriving, ScopedTypeVariables, TypeApplications,
+  FlexibleInstances #-}
+
 module Chapter12.Database where
+
+import Control.Monad.Logger
+import Control.Monad.Trans
+import Data.Int
+import Database.Persist.Sqlite
+import Database.Persist.TH
+
+share
+  [mkPersist sqlSettings, mkMigrate "migrateAll"]
+  [persistLowerCase|
+Country json
+  name String
+  canWeSend Bool default=True
+  UniqueCountryName name
+  deriving Show
+Client json
+  firstName String
+  lastName  String
+  address   String
+  country   CountryId
+  age       Int
+  UniqueClient firstName lastName address country
+  deriving Show
+Product json
+  name String
+  description String
+  price Double
+  inStock Int
+  deriving Show
+Purchase json
+  client  ClientId
+  product ProductId
+  number  Int
+  amount  Double
+  deriving Show
+|]
--- chapter12/app/Main.hs
+++ chapter12/app/Main.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell, QuasiQuotes, TypeFamilies,
   MultiParamTypeClasses, FlexibleContexts, GADTs, OverloadedStrings,
@@ -22,53 +20,42 @@ import Text.Hamlet
 import Web.Spock
 import Web.Spock.Config
 
+import Chapter12.Database
 import Text.Digestive
 import Text.Digestive.Blaze.Html5
 import Text.Digestive.Util
 import Web.Spock.Digestive
 
-share
-  [mkPersist sqlSettings, mkMigrate "migrateAll"]
-  [persistLowerCase|
-        Country json
-          name String
-          canWeSend Bool default=True
-          UniqueCountryName name
-          deriving Show
-        Client json
-          firstName String
-          lastName  String
-          address   String
-          country   CountryId
-          age       Int
-          UniqueClient firstName lastName address country
-          deriving Show
-        Product json
-          name String
-          description String
-          price Double
-          inStock Int
-          deriving Show
-        Purchase json
-          client  ClientId
-          product ProductId
-          number  Int
-          amount  Double
-          deriving Show
-|]
-
 main :: IO ()
 main = do
-  cfg <- defaultSpockCfg () PCNoDatabase ()
-  runSpock 3000 (spock cfg app)
+  Db.runSqlite "example.db" $ Db.runMigration migrateAll
+  runNoLoggingT $
+    Db.withSqlitePool "example.db" 10 $ \pool ->
+      liftIO $ do
+        cfg <- defaultSpockCfg () (PCPool pool) ()
+        runSpock 3000 (spock cfg app)
         {- HLINT ignore app -}
 
-app :: SpockM () () () ()
+app :: SpockM Db.SqlBackend () () ()
 app = do
-  get "about" $
-    html $
-    mconcat
-      ["<html><body>", " <h1>Hello Practical Haskell!</h1>", "</body></html>"]
-  hookAnyAll $ \_route -> do
-    setStatus notFound404
-    html "<h1>Not found :(</h1>"
+  get ("product" <//> var) $ \(productId :: Integer) -> do
+    product <-
+      runQuery $ \conn ->
+        flip Db.runSqlPersistM conn $
+        Db.get $ ProductKey (Db.SqlBackendKey $ fromIntegral productId)
+    case product of
+      Just (Product {..}) ->
+        html $
+        mconcat
+          [ "<html><body>"
+          , "<h1>"
+          , T.pack productName
+          , "</h1>"
+          , "<p>"
+          , T.pack productDescription
+          , "</p>"
+          , "</body></html>"
+          ]
+      Nothing -> do
+        setStatus notFound404
+        html "<h1>Not Found :(</h1>"
