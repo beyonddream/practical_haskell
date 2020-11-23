bef75109317cddeb7d65ba8d692d3d821451afe9
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-11-23 13:13:04 -0800

    update

--- chapter13/app/Chapter13/TP.hs
+++ chapter13/app/Chapter13/TP.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE DataKinds, GADTs, TypeFamilies, UndecidableInstances
-  #-}
+{-# LANGUAGE DataKinds, GADTs, TypeFamilies, UndecidableInstances,
+  TypeOperators #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Chapter13.TP where
@@ -31,4 +31,60 @@ $(promote
   min Zero _ = Zero
   min _ Zero = Zero
   min (Succ x) (Succ y) = Succ (min x y)
+  
+  data Range = Empty
+             | Open Nat
+             | Closed Nat Nat
+  
+  infinite :: Range
+  infinite = Open Zero
+  
+  data Comparison = Less'
+                  | Equal'
+                  | Greater'
+  
+  compare' :: Nat -> Nat -> Comparison
+  compare' Zero Zero = Equal'
+  compare' Zero (Succ _) = Less'
+  compare' (Succ _) Zero = Greater'
+  compare' (Succ x) (Succ y) = compare' x y
+  
+  restrictFrom :: Nat -> Range -> Range
+  restrictFrom _ Empty = Empty
+  restrictFrom n (Open f) = restrictFrom1 n f (compare' n f)
+  restrictFrom n (Closed f t)
+    = restrictFrom2 n f t (compare' n f) (compare' n t)
+  
+  restrictFrom1 :: Nat -> Nat -> Comparison -> Range
+  restrictFrom1 n _ Greater' = Open n
+  restrictFrom1 _ f Equal' = Open f
+  restrictFrom1 _ f Less' = Open f
+  
+  restrictFrom2 ::
+                Nat -> Nat -> Nat -> Comparison -> Comparison -> Range
+  restrictFrom2 _ _ _ Greater' Greater' = Empty
+  restrictFrom2 _ _ _ Greater' Equal' = Empty
+  restrictFrom2 n _ t Greater' Less' = Closed n t
+  restrictFrom2 _ f t Equal' _ = Closed f t
+  restrictFrom2 _ f t Less' _ = Closed f t
   |])
+
+data SNat (n :: Nat) where
+  SZero :: SNat Zero
+  SSucc :: SNat n -> SNat (Succ n)
+
+toNat :: SNat n -> Nat
+toNat SZero = Zero
+toNat (SSucc n) = Succ (toNat n)
+
+printDateRestriction :: Offer a r -> String
+printDateRestriction (From n _) = "From " ++ show (toNat n)
+--printDateRestriction (Until n _) = "Until " ++ show (toNat n)
+printDateRestriction _ = "No date restriction"
+
+data Offer a (r :: Range) where
+  Present :: a -> Offer a Infinite
+  PercentDiscount :: Float -> Offer a Infinite
+  AbsoluteDiscount :: Float -> Offer a Infinite
+  From :: SNat n -> Offer a d -> Offer a (RestrictFrom n d)
+  --Until :: SNat n -> Offer a d -> Offer a (RestrictTo n d)
