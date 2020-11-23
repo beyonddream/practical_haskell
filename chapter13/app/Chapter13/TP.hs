3e08ad305cac7594526f85acc9339e2b94f7356d
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-11-23 09:57:43 -0800

    add singletons example

--- chapter13/app/Chapter13/TP.hs
+++ chapter13/app/Chapter13/TP.hs
@@ -1,1 +1,34 @@
+{-# LANGUAGE DataKinds, GADTs, TypeFamilies, UndecidableInstances
+  #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Chapter13.TP where
+
+import Data.Singletons.TH hiding (Min)
+
+{-- data Nat = Zero | Succ Nat
+
+data Vect n a where
+        VNil :: Vect Zero a
+        VCons :: a -> Vect n a -> Vect (Succ n) a
+
+--type family Plus (x :: Nat) (y :: Nat) :: Nat where
+type family Plus x y where
+        Plus Zero x = x
+        Plus (Succ x) y = Succ (Plus x y)--}
+$(promote
+    [d|
+  
+  data Nat = Zero
+           | Succ Nat
+               deriving (Show, Eq)
+  
+  plus :: Nat -> Nat -> Nat
+  plus Zero y = y
+  plus (Succ x) y = Succ (plus x y)
+  
+  min :: Nat -> Nat -> Nat
+  min Zero _ = Zero
+  min _ Zero = Zero
+  min (Succ x) (Succ y) = Succ (min x y)
+  |])
