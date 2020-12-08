d56d39cd54edc2b79369e1b6060b2634c8d95ef2
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-12-07 22:13:47 -0800

    Solution Ex 14.1

--- chapter14/app/Chapter14/ExSimple.hs
+++ chapter14/app/Chapter14/ExSimple.hs
@@ -2,7 +2,8 @@ module Chapter14.ExSimple where
 
 import Chapter14.Simple
 
-executeExpr :: Expr -> String -> Int
-executeExpr e s =
-  let syn = wrap_Expr (sem_Expr e) (Inh_Expr s)  -- returns Syn_Expr
-   in result_Syn_Expr syn
+executeExpr ::
+     Ord a => Expr a -> [(a, Float)] -> (Maybe Int, Maybe Float, Maybe Bool)
+executeExpr e products =
+  let syn = wrap_Expr (sem_Expr e) (Inh_Expr products) -- returns Syn_Expr
+   in (intValue_Syn_Expr syn, fltValue_Syn_Expr syn, boolValue_Syn_Expr syn)
