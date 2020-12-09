d75ab26e3023a31e5147cc8cd898500dd2a3084f
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-12-08 16:07:50 -0800

    possibly buggy solution to ex14-2 but it "my" buggy solution.

--- chapter14/app/Chapter14/ExOffer.hs
+++ chapter14/app/Chapter14/ExOffer.hs
@@ -1,1 +1,8 @@
 module Chapter14.ExOffer where
+
+import Chapter14.Offer
+
+executeOffer :: Ord a => Offer a -> ([a], Int)
+executeOffer e =
+  let syn = wrap_Offer (sem_Offer e) Inh_Offer -- returns Syn_Offer
+   in (presents_Syn_Offer syn, maxDuration_Syn_Offer syn)
