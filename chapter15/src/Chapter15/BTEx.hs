5c7d626697a806f91a339cdd04608c9f29a0ee7e
beyonddream <43626691+beyonddream@users.noreply.github.com>
2020-12-20 16:00:43 -0800

    Solution for Ex 15-1

--- chapter15/src/Chapter15/BTEx.hs
+++ chapter15/src/Chapter15/BTEx.hs
@@ -1,10 +1,34 @@
-module Chapter15.BTEx where
+-- | Simple implementation of binary trees
+module Chapter15.BTEx
+    -- * The main data type
+  ( BinaryTree(..)
+    -- * Operations
+    -- ** Insertion
+  , treeInsert
+  , treeMerge
+  , treeFind
+  , treeSize
+  , treeDelete
+  ) where
 
+-- | A typical binary tree
 data BinaryTree a
-  = Leaf
-  | Node a (BinaryTree a) (BinaryTree a)
+  = Leaf -- ^Leaves
+  | Node a (BinaryTree a) (BinaryTree a) -- ^Inner nodes
   deriving (Show, Eq)
 
+{-|
+Inserts an element into a 'BinaryTree'
+
+ * If it finds a leaf, insert there
+
+ * If smaller than the item in the node, insert in the left
+
+ * If larger than the item in the node, insert in the right
+
+>>> treeInsert 1 Leaf
+Node 1 Leaf Leaf
+-}
 treeInsert :: Ord a => a -> BinaryTree a -> BinaryTree a
 treeInsert x Leaf = Node x Leaf Leaf
 treeInsert x (Node y l r)
@@ -14,3 +38,22 @@ treeInsert x (Node y l r)
 treeMerge :: Ord a => BinaryTree a -> BinaryTree a -> BinaryTree a
 treeMerge t Leaf = t
 treeMerge t (Node x l r) = treeInsert x $ treeMerge (treeMerge t l) r
+
+treeFind :: Ord a => a -> BinaryTree a -> Maybe a
+treeFind t (Node v l r) =
+  case compare t v of
+    EQ -> Just v
+    LT -> treeFind t l
+    GT -> treeFind t r
+treeFind _ Leaf = Nothing
+
+treeSize :: Ord a => BinaryTree a -> Int
+treeSize Leaf = 0
+treeSize (Node _ l r) = 1 + treeSize l + treeSize r
+
+treeDelete :: Ord a => a -> BinaryTree a -> BinaryTree a
+treeDelete _ Leaf = Leaf
+treeDelete x (Node y l r)
+  | x == y = treeMerge l r
+  | x < y = Node y (treeDelete x l) r
+  | otherwise = Node y l (treeDelete x r)
--- chapter15/test/Tasty.hs
+++ chapter15/test/Tasty.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Chapter15.BTEx
+import Data.Maybe
 import Test.Tasty
 import Test.Tasty.HUnit as HU
 
@@ -12,6 +13,26 @@ hunitTestInsertOnLeaf =
   HU.testCase "Insert 'a' on empty tree" $
   assertEqual "Insertion is wrong" (treeInsert 'a' Leaf) (Node 'a' Leaf Leaf)
 
+hunitTestSizeOfTree :: TestTree
+hunitTestSizeOfTree =
+  HU.testCase "Size of a tree" $
+  assertEqual
+    "Size is wrong"
+    (treeSize (treeInsert 'a' (treeInsert 'b' Leaf)))
+    2
+        {- HLINT ignore hunitTestDeleteOnTree -}
+
+hunitTestDeleteOnTree :: TestTree
+hunitTestDeleteOnTree =
+  HU.testCase "Cannot find deleted element in a tree" $
+  (isNothing $ treeFind 'a' $ treeDelete 'a' $ treeInsert 'a' Leaf) HU.@?
+  "Can find element"
+
 allTests :: TestTree
 allTests =
-  testGroup "Tasty Tests" [testGroup "HUnit Tests" [hunitTestInsertOnLeaf]]
+  testGroup
+    "Tasty Tests"
+    [ testGroup
+        "HUnit Tests"
+        [hunitTestInsertOnLeaf, hunitTestSizeOfTree, hunitTestDeleteOnTree]
+    ]
